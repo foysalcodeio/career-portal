8ac95d6a4353d192e3f114efd6cf49c0be64bd7c
Md.Foysal Ahammed <md.foysalahammedforhad@gmail.com>
2024-02-06 14:25:57 +0600

    footer done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+    <h1 className='text-2xl bg-blue-500'>
+      Hello world! f
+    </h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Root from "./Components/Root/Root";
+import Home from "./Components/Home/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
